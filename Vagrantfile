12e476024f3a4c57824a2328d206af304d7944ea
JMGoldsmith <spartanaudio@gmail.com>
2015-03-28 12:20:39 -0700

    first

---
+++ Vagrantfile
@@ -0,0 +1,20 @@
+Vagrant.configure('2') do |config|
+  config.vm.box      = 'trusty32'
+  config.vm.box_url  = 'trusty-server-cloudimg-i386-vagrant-disk1.box'
+  config.vm.hostname = 'devenv'
+
+  config.vm.provider "virtualbox" do |v|
+    v.memory = "1024"
+  end
+
+  config.vm.network "private_network", ip: "10.10.10.10"
+  config.vm.network "forwarded_port", guest: 3000, host: 3000
+  config.vm.network "forwarded_port", guest: 4567, host: 4567
+  config.vm.synced_folder ".", "/home/vagrant/code/devenv", type: "nfs"
+
+  config.vm.provision :puppet do |puppet|
+    puppet.manifests_path = 'puppet/manifests'
+    puppet.module_path    = 'puppet/modules'
+    puppet.options        = '--verbose --debug'
+  end
+end
---
+++ puppet/manifests/default.pp
@@ -0,0 +1,150 @@
+$as_vagrant   = 'sudo -u vagrant -H bash -l -c'
+$home         = '/home/vagrant'
+
+Exec {
+  path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin']
+}
+
+# --- Preinstall Stage ---------------------------------------------------------
+
+stage { 'preinstall':
+  before => Stage['main']
+}
+
+class apt_get_update {
+  exec { 'apt-get -y update':
+    unless => "test -e ${home}/.rbenv"
+  }
+}
+class { 'apt_get_update':
+  stage => preinstall
+}
+
+# --- SQLite -------------------------------------------------------------------
+
+package { ['sqlite3', 'libsqlite3-dev']:
+  ensure => installed;
+}
+
+# --- Packages -----------------------------------------------------------------
+
+package { 'curl':
+  ensure => installed
+}
+
+package { 'build-essential':
+  ensure => installed
+}
+
+package { 'git-core':
+  ensure => installed
+}
+
+package { 'vim':
+  ensure => installed
+}
+
+#rmagick dependencies.
+package { 'libmagickwand-dev':
+  ensure => installed
+}
+
+package { 'imagemagick':
+  ensure => installed
+}
+
+# Nokogiri dependencies.
+package { ['libxml2', 'libxml2-dev', 'libxslt1-dev']:
+  ensure => installed
+}
+
+# ExecJS runtime.
+package { 'nodejs':
+  ensure => installed,
+  require => Apt::Ppa['ppa:chris-lea/node.js'],
+}
+
+package { 'zsh':
+  ensure => installed
+}
+
+# --- Configuration Files ---------------------------------------------------------------------
+
+class { 'dotfiles': }
+
+# --- Ruby ---------------------------------------------------------------------
+
+class { 'rbenv': install_dir => "${home}/.rbenv" }
+#specify ruby version to be default
+$rubyver = '2.0.0-p481'
+
+#install rbenv plugins
+rbenv::plugin { ['sstephenson/ruby-build', 'rkh/rbenv-update', 'sstephenson/rbenv-gem-rehash']: }
+
+rbenv::build { $rubyver: global => true }
+rbenv::gem { 'pry': ruby_version => $rubyver }
+rbenv::gem { 'hirb': ruby_version => $rubyver }
+rbenv::gem { 'mini_magick': ruby_version => $rubyver }
+rbenv::gem { 'nokogiri': ruby_version => $rubyver }
+
+exec {'chown_rbenv':
+  command => "chown -R vagrant:vagrant ${home}/.rbenv",
+  require   => Rbenv::Build[$rubyver]
+}
+
+# --- Node ---------------------------------------------------------------------
+
+class { 'apt': }
+apt::ppa { 'ppa:chris-lea/node.js': }
+
+# directory for globally installed npm packages
+file { "${home}/.local/":
+  ensure => "directory",
+  owner => "vagrant",
+  group => "vagrant"
+}
+
+
+# --- Symlink Dir Creation -----------------------------------------------------
+
+file { "${home}/code/":
+    ensure => "directory",
+    owner => "vagrant",
+    group => "vagrant"
+}
+
+file { "${home}/code/devenv/":
+    ensure => "directory",
+    owner => "vagrant",
+    group => "vagrant"
+}
+
+# --- Zsh and Oh-My-Zsh ---------------------------------------------------------------------
+
+class { 'ohmyzsh': }
+ohmyzsh::install { 'vagrant': }
+
+# --- Postgresql -----------------------------------------------------------------
+
+class { 'postgresql::server': }
+
+package { 'libpq-dev':
+  ensure => installed,
+  require   => Class['postgresql::server']
+}
+package { 'postgresql-contrib':
+    ensure  => installed,
+    require   => Class['postgresql::server']
+}
+
+postgresql::server::role { 'vagrant':
+  createdb => true,
+  require   => Class['postgresql::server']
+}
+
+postgresql::server::db { 'vagrant':
+  user     => 'vagrant',
+  encoding => 'UTF8',
+  password => '',
+  require   => Class['postgresql::server']
+}
